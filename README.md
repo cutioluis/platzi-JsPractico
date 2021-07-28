d6c8aaeb5f025916a6996f3d80a7da70c7f13936
Luis Miguel Cutiopala <61071133+cutioluis@users.noreply.github.com>
2021-07-27 19:08:51 -0500

    ADD innerText para usar el js para pintar en el HTML

--- README.md
+++ README.md
@@ -2,8 +2,18 @@
 [![Platzi Math](https://raw.githubusercontent.com/cutioluis/platzi-JsPractico/master/assets/banner.png "Platzi Math")](https://raw.githubusercontent.com/cutioluis/platzi-JsPractico/master/assets/banner.png "Platzi Math")
 ❤ Un curso increible donde aprenderas de una manera practica a utilizar Js, instruido por el genial @juandc que te enseñara de una manera super divertida a usar javascript el lenguaje mas popular y mejor pagado 💰 del mundo 
 
-## Iniciar 🚛
-Clonar la Aplicacion 
+## Taller 1: Figuras Geometricas 🚛
+- Primer paso: definir las fòrmulas 
+- Segundo paso: implrementar la fòrmulas en Js
+- Tercer paso: crear funciones
+- Cuarto paso: inetegrar JS con HTML
+
+
+## Taller 2: Porcentajes y Descuentos 
+- Primer paso: definir las fòrmulas 
+- Segundo paso: implrementar la fòrmulas en Js
+- Tercer paso: crear funciones
+- Cuarto paso: inetegrar JS con HTML
 
 ## Construido con  🛠️
 - Css
--- index.js
+++ index.js
@@ -22,35 +22,69 @@ function areaTriangulo(altura, base) {
 
 // Funciones que encapsulan las funcionalidades de círculo
 function diametroCirculo(radio) {
-  return radio > 0 ? radio * 2 
-  : "Ingresar un valor válido";
+  return radio > 0 ? radio * 2 : "Ingresar un valor válido";
 }
 
 function perimetroCirculo(radio) {
   const diametro = diametroCirculo(radio);
-  return radio > 0 ? diametro * Math.PI
-  : "Ingresar un valor válido";
+  return radio > 0 ? diametro * Math.PI : "Ingresar un valor válido";
 }
 
 function areaCirculo(radio) {
-  return radio > 0 ? radio * radio * Math.PI 
-  : "Ingresar un valor válido";
+  return radio > 0 ? radio * radio * Math.PI : "Ingresar un valor válido";
 }
 
+/* Altura de un triángulo isósceles.  */
+
+function funcionIsoceles(lado1, lado2, base) {
+  if (lado1 === lado2 && lado1 != base) {
+    alert("Triangulo Isósceles");
+  } else {
+    alert("No es");
+  }
+}
 
 /* Interaccion HTML */
 
 function calcularPerimetroCuadrado(params) {
-  const input = document.getElementById("InputCuadrado")
-  const value = input.value
+  const input = document.getElementById("InputCuadrado");
+  const value = input.value;
 
-  const perimetro = perimetroCuadrado(value)
-  alert(perimetro)
+  const perimetro = perimetroCuadrado(value);
+    
+  const ResultPerimetro = document.getElementById("ResultPerimetro")
+  ResultPerimetro.innerText = "El perimetro es " + perimetro
 }
 function calcularAreaCuadrado(params) {
-  const input = document.getElementById("InputCuadrado")
-  const value = input.value
+  const input = document.getElementById("InputCuadrado");
+  const value = input.value;
 
-  const area = areaCuadrado(value)
-  alert(area)
+  const area = areaCuadrado(value);
+  
+  const ResultArea = document.getElementById("ResultArea")
+  ResultArea.innerText = "El area es " + area
+}
+
+/* Porcemtajes y Descuentos */
+function calcularPrecioConDescuento(precio, descuento) {
+  const porcentajePrecioConDescuento = 100 - descuento;
+  const precioConDescuento =
+    (precio * porcentajePrecioConDescuento) / 100;
+    return precioConDescuento
+}
+
+calcularPrecioConDescuento(300, 30)
+
+
+function calcDiscountPrice(params) {
+  const inputPrice = document.getElementById("InputPrice");
+  const priceValue = inputPrice.value
+
+  const InputDiscount = document.getElementById("InputDiscount");
+  const discountValue = InputDiscount.value
+
+  const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue)
+
+  const ResultPrice = document.getElementById("ResultPrice")
+  ResultPrice.innerText = "El precio con descuento son $" + precioConDescuento
 }
